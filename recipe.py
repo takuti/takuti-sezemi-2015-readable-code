88be1b8e067ca6e48f8673a58066f7c167a1f799
kabayan55 <g1120513@is.ocha.ac.jp>
2015-06-06 15:13:30 +0900

    get recipe id
    idを得るとそのidのレシピをかえす。id未入力のものに対しては未対応

--- recipe.py
+++ recipe.py
@@ -4,10 +4,11 @@ import sys
 
 def main():
   argc = len(sys.argv)
-  if argc != 2:
-    sys.exit('Error: `$ python recipe.py ${recipe-filename}`')
+  if argc != 3:
+    sys.exit('Error: `$ python recipe.py ${recipe-filename}` (recipe_id)')
 
   recipe_filename = sys.argv[1]
+  get_recipe_id = sys.argv[2]
 
   # read/print all recipes from recipe-data-file
   try:
@@ -16,7 +17,12 @@ def main():
     recipes = map(lambda recipe: recipe.rstrip(), recipe_file.readlines())
     recipe_id=1
     for recipe in recipes:
-      print str(recipe_id)+': '+recipe
+      #print 'get_recipe_id',get_recipe_id
+      #print 'recipe_id',recipe_id
+      #print type(get_recipe_id)
+      #print type(recipe_id)
+      if int(get_recipe_id) == recipe_id: 
+        print str(recipe_id)+': '+recipe
       recipe_id+=1
     recipe_file.close()
   except IOError:
