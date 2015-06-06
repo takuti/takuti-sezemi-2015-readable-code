ae957ed94b5832823db06ab81d2f578695880a33
takuti <k.takuti@gmail.com>
2015-06-06 11:35:32 +0900

    spec3: add error handling for fileIO

--- recipe.py
+++ recipe.py
@@ -10,11 +10,15 @@ def main():
   recipe_filename = sys.argv[1]
 
   # read/print all recipes from recipe-data-file
-  with open(recipe_filename, 'r') as f:
+  try:
+    f = open(recipe_filename, 'r')
     # cut the tail '\n', and create recipe list
     recipes = map(lambda recipe: recipe.rstrip(), f.readlines())
     for r in recipes:
       print r
+  except IOError:
+    print 'IOError: file `%s` does not exist' % recipe_filename
+
 
 if __name__ == '__main__':
   main()
