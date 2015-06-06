9f5055cba2e9179f202becd29b77d103c699e74d
takuti <k.takuti@gmail.com>
2015-06-06 11:20:14 +0900

    spec3: done implement

--- recipe.py
+++ recipe.py
@@ -1,8 +1,20 @@
 # coding: utf-8
 
+import sys
+
 def main():
-  # spec1
-  print 'オムライス'
+  argc = len(sys.argv)
+  if argc != 2:
+    sys.exit('Error: `$ python recipe.py ${recipe-filename}`')
+
+  recipe_filename = sys.argv[1]
+
+  # read/print all recipes from recipe-data-file
+  with open(recipe_filename, 'r') as f:
+    # cut the tail '\n', and create recipe list
+    recipes = map(lambda recipe: recipe.rstrip(), f.readlines())
+    for r in recipes:
+      print r
 
 if __name__ == '__main__':
   main()
